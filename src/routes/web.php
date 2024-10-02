fb5dceab2e47091f38b70fadfc8722ddcaee7f17
younghyeong ryu <wangta69@naver.com>
2024-10-02 11:36:32 +0900

    t

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,5 @@
 <?php
-Route::group(['prefix' => 'delivery-tracking', 'as' => 'delivery.tracking.', 'namespace' => 'Pondol\DeliveryTracking\Https\Controllers', 'middleware' => ['web']], function () {
+Route::group(['prefix' => 'delivery-tracking', 'as' => 'delivery.tracking.', 'namespace' => 'Pondol\DeliveryTracking\Http\Controllers', 'middleware' => ['web']], function () {
   Route::get('', array('uses'=>'DeliveryTrackingController@search'));
   Route::get('couriers', array('uses'=>'DeliveryTrackingController@couriers'));
   Route::get('{courier}/{invoicenumber}/{type?}', array('uses'=>'DeliveryTrackingController@tracking'));
